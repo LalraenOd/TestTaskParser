a1d3cefe90fcbfb20a4809de5f5b76be58e68984
LalraenOd <minenko.od@gmail.com>
2019-02-13 16:55:06 +0200

    Parses rebuild

--- TestTaskParser/Program.cs
+++ TestTaskParser/Program.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Configuration;
-using System.Data.SqlClient;
-using System.Text.RegularExpressions;
-using System.Net;
 using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace TestTaskConsoleApp
@@ -16,82 +11,95 @@ namespace TestTaskConsoleApp
     {
         static void Main(string[] args)
         {
-            PartNumberGet();
+            string[] partsToParseManual = new string[] { "109901140", "230235220", "230701701" };
+            PartParser(partsToParseManual);
+            //PartNumberGet();
             Console.Read();
         }
 
         public static void PartNumberGet()
         {
+            Console.WriteLine("Reading File");
             string[] PartCodes = File.ReadAllLines("PartCodes.txt");
+
+            //Запуск парсеров в 4 потоках
+            Thread[] parsers = new Thread[4];
+            for (int i = 0; i < parsers.Length; i++)
+            {
+                parsers[i] = new Thread ( () => PartParser(PartCodes) );
+                parsers[i].Start();
+            }
         }
 
-        public void PartParser(string[] partNumbers)
+        public static void PartParser(string[] partNumbers)
         {
-            //Генерация ссылки
-            string siteToParse = "https://otto-zimmermann.com.ua/autoparts/product/ZIMMERMANN/";
-
             foreach (var partnumber in partNumbers)
-            {
+            {            
+                //Генерация ссылки
+                string siteToParse = "https://otto-zimmermann.com.ua/autoparts/product/ZIMMERMANN/";
                 siteToParse += partnumber + @"/";
-            }
+                
+                //скачивание HTML
+                Console.WriteLine("\nGetting HTML from:\n{0}", siteToParse);
+                WebClient client = new WebClient
+                {
+                    Encoding = Encoding.UTF8
+                };
+                string downloadedHTML = client.DownloadString(siteToParse);
 
-            //скачивание HTML
-            Console.WriteLine("Getting HTML from:\n{0}", siteToParse);
-            WebClient client = new WebClient
-            {
-                Encoding = Encoding.UTF8
-            };
-            string downloadedHTML = client.DownloadString(siteToParse);
+                //Парсинг
+                Console.WriteLine("Trying to parse...");
 
-            //Парсинг
-            Console.WriteLine("Trying to parse...");
-
-            string[] patterns = new string[]
-            {
+                string[] patterns = new string[]
+                {
                 "<td class=ProdBra>(?<result>.+)</td>"
                 ,"<td class=ProdArt>(?<result>.+)"
                 ,"<td class=ProdName>(?<result>.+)</td>"
-                //,"^<div class=partsDescript>.*$</div></div>"
-                //,"<td><a href = \"(?<result>.+)\" > 7H0 615 301 E</a></td>"
-                //,"<td class=\"tarig\">(?<result>.+)</td>"
-                //,"<a href=\"(?<result>.+)\">7H0 615 301 E</a>"
-                //,"<span class=\"artkind_original\">(?<result>.+)</span>"
-                //,"<span class=criteria>.+</span><br>"
-            };
+                    //,"^<div class=partsDescript>.*$</div></div>"
+                    //,"<td><a href = \"(?<result>.+)\" > 7H0 615 301 E</a></td>"
+                    //,"<td class=\"tarig\">(?<result>.+)</td>"
+                    //,"<a href=\"(?<result>.+)\">7H0 615 301 E</a>"
+                    //,"<span class=\"artkind_original\">(?<result>.+)</span>"
+                    //,"<span class=criteria>.+</span><br>"
+                };
 
-            string[] result = new string[patterns.Length + 1];
-            result[0] = siteToParse;
+                string[] result = new string[patterns.Length + 1];
+                result[0] = siteToParse;
 
-            for (int patternNumber = 0; patternNumber < patterns.Length; patternNumber++)
-            {
-                Regex regex = new Regex(patterns[patternNumber]);
-                MatchCollection matches = regex.Matches(downloadedHTML);
-                if (matches.Count > 0)
+                for (int patternNumber = 0; patternNumber < patterns.Length; patternNumber++)
                 {
-                    foreach (Match match in matches)
+                    Regex regex = new Regex(patterns[patternNumber]);
+                    MatchCollection matches = regex.Matches(downloadedHTML);
+                    if (matches.Count > 0)
                     {
-                        if (patternNumber != 3)
-                            result[patternNumber + 1] = match.Groups["result"].Value.ToString();
-                        else
-                            result[patternNumber + 1] += match;
+                        foreach (Match match in matches)
+                        {
+                            if (patternNumber != 3)
+                                result[patternNumber + 1] = match.Groups["result"].Value.ToString();
+                            else
+                                result[patternNumber + 1] += match;
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("No matches on pattern " + patterns[patternNumber].ToString());
                     }
                 }
-                else
+                Logger(siteToParse);
+
+
+                Console.WriteLine("Result:");
+                foreach (var str in result)
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("No matches on pattern " + patterns[patternNumber].ToString());
-                    Console.Read();
+                    if (str != null)
+                        Console.WriteLine(str.ToString());
+                    else
+                        Console.WriteLine("Not found");
                 }
             }
-            Logger(siteToParse);
 
-            foreach (var str in result)
-            {
-                if(str != null)
-                    Console.WriteLine("\nResult:\n" + str.ToString());
-                else
-                    Console.WriteLine("\nResult:\nNULL");
-            }
+            
         }
 
         public static void Logger(string parsedLink, bool parseSuccessed = true)
