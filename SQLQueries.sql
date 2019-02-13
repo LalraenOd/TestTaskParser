6f53edf1d93c7d4e13a4333d02edefb7e7b5b690
LalraenOd <minenko.od@gmail.com>
2019-02-13 14:49:32 +0200

    New SQL code

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -1,13 +1,27 @@
 USE TestCaseDb
 GO
 
-
-CREATE TABLE Parts(
-
-)
-
+IF OBJECT_ID('Parts') IS NOT NULL
+	DROP TABLE Parts
 GO
 
-CREATE TABLE LinkParts
+IF OBJECT_ID('LinkParts') IS NOT NULL
+	DROP TABLE LinkParts
+GO	
 
+CREATE TABLE LinkParts(
+Id int IDENTITY PRIMARY KEY NOT NULL,
+PartNumber varchar(20)
+);
+GO
+
+CREATE TABLE Parts(
+Id int IDENTITY PRIMARY KEY NOT NULL,
+URL varchar(70),
+ArtNumber varchar(20),
+BrandName nvarchar(20),
+PartName nvarchar(30),
+Specs xml,
+LinkedParts int FOREIGN KEY REFERENCES LinkParts(Id)
+);
 GO
